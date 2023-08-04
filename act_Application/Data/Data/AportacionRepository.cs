da7853f63f73766f3dca18110975382b80985c78
CesarG97 <cgracia@xtrim.com.ec>
2023-08-04 16:50:12 -0500

    Tabla de Aportaciones (bases de diseño y logica de programacion)

---
+++ act_Application/Data/Data/AportacionRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using act_Application.Helper;
+using MySql.Data.MySqlClient;
+
+namespace act_Application.Data
+{
+    public class AportacionRepository
+    {
+
+        public int GetTotalAportaciones()
+        {
+            string connectionString = AppSettingsHelper.GetConnectionString();
+            try
+            {
+                string query = ConfigReader.GetQuery("SelectAportaciones");
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+                        return count;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Maneja las excepciones aquí si es necesario
+                return -1; // Valor negativo para indicar un error
+            }
+        }
+    }
+}
