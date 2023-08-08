40a2fc8580d1f53d65d0956f67087f1e6d15c06c
CesarG97 <cgracia@xtrim.com.ec>
2023-08-08 16:51:14 -0500

    Modificaciones css

--- act_Application/Logica/MetodoAportaciones.cs
+++ act_Application/Logica/MetodoAportaciones.cs
@@ -1,10 +1,7 @@
 ﻿using act_Application.Helper;
 using act_Application.Models;
 using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 
 namespace act_Application.Logica
 {
