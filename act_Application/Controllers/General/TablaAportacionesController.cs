ff7477242c160cc1e3907d84c88b156f3d7b277e
CESAR GRACIA <cesar.gracia@pucese.edu.ec>
2023-08-07 23:22:58 -0500

    tabla con valores repetados por fechas

--- act_Application/Controllers/General/TablaAportacionesController.cs
+++ act_Application/Controllers/General/TablaAportacionesController.cs
@@ -1,16 +1,16 @@
-﻿using act_Application.Logica;
-using Microsoft.AspNetCore.Mvc;
+﻿    using act_Application.Logica;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace act_Application.Controllers.General
-{
-    public class TablaAportacionesController : Controller
+    namespace act_Application.Controllers.General
     {
-        public IActionResult Index()
+        public class TablaAportacionesController : Controller
         {
-            var metodoAportaciones = new MetodoAportaciones();
-            var aportaciones = metodoAportaciones.ObtenerAportaciones();
+            public IActionResult Index()
+            {
+                var metodoAportaciones = new MetodoAportaciones();
+                var aportaciones = metodoAportaciones.ObtenerAportaciones();
 
-            return View(aportaciones);
+                return View(aportaciones);
+            }
         }
     }
-}
