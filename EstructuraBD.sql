dec259eca0c2e4b93ebb0001891471e8ac254d6e
CesarG97 <cgracia@xtrim.com.ec>
2023-08-24 12:00:32 -0500

    Vista de Aportes Parte 2 (Cambios en la BD)

--- EstructuraBD.sql
+++ EstructuraBD.sql
@@ -7,130 +7,85 @@ Scaffold-DbContext "server=192.168.21.193; port=3306; database=desarrollo; uid=c
 
 CREATE DATABASE `desarrollo`;
 
-/*Tabla de Usuarios*/
-CREATE TABLE `desarrollo`.`act_User` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Cedula` VARCHAR(10) NOT NULL,
-
-  `Correo` VARCHAR(45) NOT NULL,
-  `NombreYApellido` VARCHAR(45) NOT NULL,
-  `Celular` INT NOT NULL,
-  `Contrasena` VARCHAR(75) NOT NULL,
-  `TipoUser` VARCHAR(45) NOT NULL,
-  `NCedAccionario` INT NOT NULL,
+/*Tabla Roles*/
+CREATE TABLE `act_Rol` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `NombreRol` varchar(45) NOT NULL,
+  `DescripcionRol` varchar(45) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC),
-  UNIQUE INDEX `Cedula_UNIQUE` (`Cedula` ASC),
-  UNIQUE INDEX `CBancaria_UNIQUE` (`CBancaria` ASC))
-COMMENT = 'Tabla de Usuarios';
+  UNIQUE KEY `Id_UNIQUE` (`Id`),
+  UNIQUE KEY `NombreRol_UNIQUE` (`NombreRol`)
+) COMMENT='Tabla de Roles';
 
-/*Tabla de Roles*/
-CREATE TABLE `desarrollo`.`act_Rol` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `NombreRol` VARCHAR(45) NOT NULL,
-  `Descripcion` VARCHAR(45) NOT NULL,
+/*Tabla de Roles de Usuario*/
+CREATE TABLE `act_RolUser` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `IdUser` int(11) NOT NULL,
+  `IdRol` int(11) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC),
-  UNIQUE INDEX `NombreRol_UNIQUE` (`NombreRol` ASC),
-  UNIQUE INDEX `Descripcion_UNIQUE` (`Descripcion` ASC))
-COMMENT = 'Tabla de Roles';
-
-/*Tabla relacion Usuario-Rol*/
-CREATE TABLE `desarrollo`.`act_RolUser` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC))
-COMMENT = 'Tabla Relacion Rol Usuario';
-
-
-/* Relacion */
-/* RolUser-User */
-ALTER TABLE act_RolUser
-	ADD COLUMN IdUser INT NOT NULL;
-ALTER TABLE act_RolUser
-	ADD CONSTRAINT fk_RolUser_User
-	FOREIGN KEY (IdUser) REFERENCES act_User (Id);
-/* RolUser-Rol */
-ALTER TABLE act_RolUser
-	ADD COLUMN IdRol INT NOT NULL;
-ALTER TABLE act_RolUser
-	ADD CONSTRAINT fk_RolUser_Rol
-	FOREIGN KEY (IdRol) REFERENCES act_Rol (Id);
-
-
-/*Tabla de Transacciones*/
-CREATE TABLE `desarrollo`.`act_Transacciones` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Razon` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC))
-COMMENT = 'Tabla de Transacciones';
-
-
-/* Relacion */
-/* Transacciones-User */
-ALTER TABLE act_Transacciones
-	ADD COLUMN IdUser INT NOT NULL;
-ALTER TABLE act_Transacciones
-	ADD CONSTRAINT fk_Transacciones_User
-	FOREIGN KEY (IdUser) REFERENCES act_User (Id);
-/* Transacciones-Referentes */
-ALTER TABLE act_Transacciones
-	ADD COLUMN IdRef INT NOT NULL;
-ALTER TABLE act_Transacciones
-	ADD CONSTRAINT fk_Transacciones_Referencias
-	FOREIGN KEY (IdRef) REFERENCES act_Referencias (Id);
-
+  UNIQUE KEY `Id_UNIQUE` (`Id`),
+  KEY `fk_RolUser_User` (`IdUser`),
+  KEY `fk_RolUser_Rol` (`IdRol`)
+) COMMENT='Tabla Relacion Rol Usuario';
 
 /*Tabla de Aportaciones*/
-CREATE TABLE `desarollo`.`act_Aportaciones` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Razon` VARCHAR(45) NOT NULL,
-  `FechaAportacion` DATE NOT NULL,
-  `Aprobacion` VARCHAR(10) NOT NULL,
-  `CapturaPantalla` BLOB NOT NULL,
-  `CBancaria` VARCHAR(15) NOT NULL,
-  `NBanco` VARCHAR(45) NOT NULL,
+CREATE TABLE `act_Aportaciones` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Razon` varchar(45) NOT NULL,
+  `Valor` decimal(10,2) NOT NULL,
+  `IdUser` int(11) NOT NULL,
+  `FechaAportacion` date NOT NULL,
+  `Aprobacion` varchar(10) NOT NULL,
+  `Cuadrante1` int(1) NOT NULL,
+  `Cuadrante2` int(1) NOT NULL,
+  `NBanco` varchar(45) NOT NULL,
+  `CBancaria` varchar(15) NOT NULL,
+  `CapturaPantalla` longblob NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC))
-COMMENT = 'Tabla de Aportaciones Economicas';
+  UNIQUE KEY `Id_UNIQUE` (`Id`),
+  KEY `fk_Aportaciones_User` (`IdUser`)
+) COMMENT='Aportaciones ec';
 
 /*Tabla de Multas*/
-CREATE TABLE `desarrollo`.`act_Multa` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `act_Multas` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `IdUser` int(11) NOT NULL,
+  `Porcentaje` decimal(10,2) NOT NULL,
+  `Valor` decimal(10,2) NOT NULL,
+  `FechaMulta` date NOT NULL,
+  `IdAportacion` int(11) NOT NULL,
+  `Cuadrante1` int(1) NOT NULL,
+  `Cuadrante2` int(1) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Id_UNIQUE` (`Id` ASC))
-COMMENT = 'Tabla de Multas';
+  UNIQUE KEY `Id_UNIQUE` (`Id`),
+  KEY `fk_Multas_Aportaciones` (`IdAportacion`),
+  KEY `fk_Multas_User` (`IdUser`)
+) COMMENT='Tabla de Multas';
+ /*Tabla de Transacciones*/
+CREATE TABLE `act_Transacciones` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Razon` varchar(45) NOT NULL,
+  `IdUser` int(11) NOT NULL,
+  `Valor` decimal(10,2) NOT NULL,
+  `Estado` varchar(45) NOT NULL,
+  `FechDesPago` date NOT NULL,
+  `FechPagoTotalPrestamo` date NOT NULL,
+  `FechaIniCoutaPrestamo` date NOT NULL,
+  `TipoCuota` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id_UNIQUE` (`Id`),
+  KEY `fk_Transacciones_User` (`IdUser`)
+) COMMENT='Operaciones de Referentes';
 
-/* Relacion */
-/* Aportaciones-User */
-ALTER TABLE act_Aportaciones
-	ADD COLUMN IdUser INT NOT NULL,
-    ADD COLUMN Valor DECIMAL(10, 2) NOT NULL;
-ALTER TABLE act_Aportaciones
-	ADD CONSTRAINT fk_Aportaciones_User
-	FOREIGN KEY (IdUser) REFERENCES act_User (Id);
-
-/* Relacion */
-/* Multa-User */
-ALTER TABLE act_Multas
-	ADD COLUMN IdUser INT NOT NULL,
-    ADD COLUMN Porcentaje DECIMAL(10, 4) NOT NULL;
-ALTER TABLE act_Multa
-	ADD CONSTRAINT fk_Multas_User
-	FOREIGN KEY (IdUser) REFERENCES act_User (Id);
-
-
-/* Relacion */
-/* Multa-Aportaciones */
-ALTER TABLE act_Multas
-	ADD COLUMN IdAportacion INT NOT NULL;
-ALTER TABLE act_Multas
-	ADD CONSTRAINT fk_Multas_Aportaciones
-	FOREIGN KEY (IdAportacion) REFERENCES act_Aportaciones (Id);
-
-  /*Relacion*/
-  /*act_User IdSocio - Id*/
-  ALTER TABLE act_User
-    ADD CONSTRAINT fk_UserSocio_UserId
-    FOREIGN KEY (IdSocio) REFERENCES act_User (Id);
+/*Tabla Cuotas*/
+CREATE TABLE `act_Cuotas` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `ValorCuota` decimal(10,2) NOT NULL,
+  `FechaCuota` varchar(45) NOT NULL,
+  `Estado` varchar(45) NOT NULL,
+  `IdUser` int(11) NOT NULL,
+  `IdTransaccion` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_Cuotas_User` (`IdUser`),
+  KEY `fk_Cuotas_Transacciones` (`IdTransaccion`)
+) COMMENT='Tabla de Cuotas, aqui se almacena el Id del Usuario, el Id d';
