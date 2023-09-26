d8c7e825b18d65f6a088a5b6fdbada125a6bb9a3
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2023-09-26 09:42:57 -0500

    Vista Menu Principal Parte 4 (Consulta de Eventos)

---
+++ act_Application/Data/Data/EventosRepository.cs
@@ -0,0 +1,33 @@
+﻿using act_Application.Helper;
+using MySql.Data.MySqlClient;
+
+namespace act_Application.Data.Data
+{
+    public class EventosRepository
+    {
+        public int GetTotalEventos()
+        {
+            string connectionString = AppSettingsHelper.GetConnectionString();
+            try
+            {
+                string query = ConfigReader.GetQuery("SelectExistsEventos");
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+                        return count;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hubo un problema al momento de realizar la consulta de existencia de Eventos e Participacion.");
+                Console.WriteLine("Detalles del error: " + ex.Message);
+                return -1; // Valor negativo para indicar un error
+            }
+        }
+
+    }
+}
---
+++ act_Application/Logic/MetodoParticipantes.cs
@@ -0,0 +1,6 @@
+﻿namespace act_Application.Logic
+{
+    public class MetodoParticipantes
+    {
+    }
+}
