928ec5303c03e7685b4eb2f0aaae8e97653978bc
Cesar Gracia <cesar.gracia@pucese.edu.ec>
2023-09-28 16:58:25 -0500

    Reestructuracion de Kuerys (Aportaciones)

--- act_Application/Data/Data/AportacionRepository.cs
+++ act_Application/Data/Data/AportacionRepository.cs
@@ -1,11 +1,29 @@
 ﻿using act_Application.Helper;
+using act_Application.Models.BD;
 using MySql.Data.MySqlClient;
 
 namespace act_Application.Data
 {
     public class AportacionRepository
     {
+        public int TotalAportaciones { get; set; }
+        public List <ActAportacione> Aportacion { get; set; }
+        public AportacionRepository GetDataAportaciones()
+        {
+            string connectionString = AppSettingsHelper.GetConnectionString();
+            AportacionRepository result = new AportacionRepository();
+            try
+            {
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hubo un error en la consulta de eventos.");
+                Console.WriteLine("Detalles del error: " + ex.Message);
+                result.TotalAportaciones = -1; // Valor negativo para indicar un error
+            }
+
+        }
         public int GetTotalAportaciones()
         {
             string connectionString = AppSettingsHelper.GetConnectionString();
