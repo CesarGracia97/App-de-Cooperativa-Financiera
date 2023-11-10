f595446d782547e5e15475d16cedb1a132e5eb25
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-10 10:51:27 -0500

    Objetivo 2 - Transacciones 1. Layout JS. 2. Logo 3. Logout

---
+++ act_Application/wwwroot/js/General/Shared/_Layout/Logout.js
@@ -0,0 +1,23 @@
+﻿
+var inactivityTimeout = 900000; //900000 15 minutos; 60000 es un minuto en milisegundos
+
+var inactivityTimer;
+
+function resetInactivityTimer() {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = setTimeout(logout, inactivityTimeout);
+}
+
+function logout() {
+    // Simular un clic en el botón de cierre de sesión
+    document.getElementById("btn-CerrarSesion").click();
+}
+
+$(document).on('mousemove keydown', function () {
+    resetInactivityTimer();
+});
+
+// Iniciar el temporizador de inactividad al cargar la página
+$(document).ready(function () {
+    resetInactivityTimer();
+});
