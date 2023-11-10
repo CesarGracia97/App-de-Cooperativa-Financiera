80f2b30037c64bb672d464e0db179ec9bfb1f486
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-10 11:01:05 -0500

    Objetivo 2 - Transacciones 1. Controlador 2. Vista

---
+++ act_Application/Controllers/General/TransaccionesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace act_Application.Controllers.General
+{
+    public class TransaccionesController : Controller
+    {
+        public IActionResult Transaccion()
+        {
+            return View();
+        }
+    }
+}
