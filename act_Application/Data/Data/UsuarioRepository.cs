2beda77d1a316b609805501b66ef7299419173d9
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-16 16:08:53 -0500

    Objetivo 2 - Transacciones 1. EmailSendServices a punto de completar.

--- act_Application/Data/Data/UsuarioRepository.cs
+++ act_Application/Data/Data/UsuarioRepository.cs
@@ -3,6 +3,7 @@ using act_Application.Models.BD;
 using act_Application.Models.Sistema.Complementos;
 using MySql.Data.MySqlClient;
 using System.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace act_Application.Data.Data
 {
@@ -105,6 +106,38 @@ namespace act_Application.Data.Data
                 }
             }
             return users;
+        }
+        public string CorreoUser(int IdUser)
+        {
+            string email = string.Empty;
+            string connectionString = AppSettingsHelper.GetConnectionString();
+            try
+            {
+                string Query = ConfigReader.GetQuery(2, "SelectCorreoUser");
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand(Query, connection);
+                    cmd.Parameters.AddWithValue("@IdUser", IdUser);
+                    cmd.CommandType = CommandType.Text;
+
+                    connection.Open();
+
+                    using (MySqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            email = Convert.ToString(rd["Correo"]);
+                        };
+                    }
+                }
+                return email;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hubo un problema al actualizar el campo de Captura de Pantalla de Cuota.");
+                Console.WriteLine("Razon del Error: ",ex);
+            }
+            return email;
         }
     }
 }
--- act_Application/Services/EmailSendServices.cs
+++ act_Application/Services/EmailSendServices.cs
@@ -3,6 +3,7 @@ using act_Application.Models.Sistema.Complementos;
 using System.Net.Mail;
 using System.Net;
 using System.Security.Claims;
+using act_Application.Data.Data;
 
 namespace act_Application.Services
 {
@@ -11,61 +12,51 @@ namespace act_Application.Services
         /*Envia la solicitud de prestamo*/
         public async Task EnviarCorreoAdmin(int opcion, int IdUser, string Descripcion)
         {
-            string correoDestino = string.Empty, subject = string.Empty, body = string.Empty;
+            string correoDestino = CorreoHelper.ObtenerCorreoDestino(), subject = string.Empty, body = string.Empty;
             switch (opcion)
             {
                 case 1:
                     //Nuevo Usuario
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Solicitud de Ingreso.";
                     body = Descripcion;
-                    await EnviarCorreo(correoDestino, subject, body);
                     break;
                  case 2:
                     //Aportaciones
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Aportacion Reciente.";
                     body = Descripcion;
                     break;
                 case 3:
                     //Pago de Multas
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Multa Cancelada Recientemente.";
                     body = Descripcion;
                     break;
                 case 4:
                     //Pago de Multas
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Multa Abonada  Recientemente.";
                     body = Descripcion;
                     break;
                 case 5:
                     //Pago de Cuotas
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Cuota Cancelada  Recientemente.";
                     body = Descripcion;
                     break;
                 case 6:
                     //Pago de Cuotas
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Cuota Abonada  Recientemente.";
                     body = Descripcion;
                     break;
                 case 7:
                     //Peticion de Prestamos Fase 1
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Solicitud de Prestamo (FASE 1)";
                     body = Descripcion;
                     break;
                 case 8:
                     //Peticion de Prestamos Fase 2
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Solicitud de Prestamo (FASE 2)";
                     body = Descripcion;
                     break;
                 case 9:
                     //Finalizacion de Evento de Participacion
-                    correoDestino = CorreoHelper.ObtenerCorreoDestino();
                     subject = "act - Application: Evento de Participacion (FINALIZADO)";
                     body = Descripcion;
                     break;
@@ -77,14 +68,33 @@ namespace act_Application.Services
 
         }
 
-        private async Task EnviarCorreoUsuario(string Descripcion)
+        public async Task EnviarCorreoUsuario(int IdUser, int opcion, string Descripcion)
         {
-            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
-            if (!string.IsNullOrEmpty(userEmail))
+            var uobj = new UsuarioRepository();
+            string userEmail = uobj.CorreoUser(IdUser), subject = string.Empty;
+            if (userEmail != null)
             {
-                var subject = "Solicitud de Prestamo (FASE 1)";
-                var body = Descripcion;
-                await EnviarCorreo(userEmail, subject, body);
+                switch (opcion)
+                {
+                    case 1:
+                        //Solicitud de Prestamos Fase 1
+                        subject = "act - Application: Solicitud de Prestamo (FASE 1)";
+                        break;
+                    case 2:
+                        //Solicitud de Prestamos Fase 2
+                        subject = "act - Application: Solicitud de Prestamo (FASE 2)";
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("EnviarCorreoUsuario - EmailSendServices. Opcion inexistente");
+                        break;
+                }
+                await EnviarCorreo(userEmail, subject, Descripcion);
+            }
+            else
+            {
+                Console.WriteLine("EnviarCorreoUsuario - EmailSendServices. Error al obtener el Correo");
             }
         }
 
