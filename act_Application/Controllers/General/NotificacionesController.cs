1fe9fd803e0c20a243b1c77a56b2292c0433c5b6
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-16 17:37:52 -0500

    Objetivo  2 - Transacciones (COMPLETADO) 1. Complementos (VM)

---
+++ act_Application/Controllers/General/NotificacionesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace act_Application.Controllers.General
+{
+    public class NotificacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ act_Application/Models/Sistema/ViewModel/Transacciones_VM.cs
@@ -0,0 +1,19 @@
+﻿using act_Application.Models.BD;
+
+namespace act_Application.Models.Sistema.ViewModel
+{
+    public class Transacciones_VM
+    {
+        public Transacciones_VM()
+        {
+            Aportaciones = new ActAportacione();
+            Prestamos = new ActPrestamo();
+            Cuotas = new ActCuota();
+            Multas = new ActMulta();
+        }
+        public ActAportacione Aportaciones { get; set; }
+        public ActPrestamo Prestamos { get; set; }
+        public ActCuota Cuotas {  get; set; }
+        public ActMulta Multas {  get; set; }
+    }
+}
