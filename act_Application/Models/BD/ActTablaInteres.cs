52041caa036f6a66c381408e711a48b8e54e2998
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-23 12:42:48 -0500

    Objetivo 3 - Eventos 1. Tabla Modelo Intereses

---
+++ act_Application/Models/BD/ActTablaInteres.cs
@@ -0,0 +1,10 @@
+﻿namespace act_Application.Models.BD;
+public partial class ActTablaInteres
+{
+    public int Id { get; set; }
+    public int IdUser { get; set; }
+    public string IdPersonalizado { get; set; }
+    public string Porcentaje { get; set; }
+    public decimal Valor { get; set; }
+    public string Estado { get; set; }
+}
