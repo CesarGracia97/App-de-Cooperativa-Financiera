fbd2651a0cde41de1e54eee6a9b5f954a94002a6
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-28 14:19:48 -0500

    Objetivo 4 - Vistas. 1. Desarrollo de la vista de tabla de Aportantes 2. Desarrollo de la Vista de tabla de Multados.

---
+++ act_Application/Controllers/General/TablaAportantesController.cs
@@ -0,0 +1,14 @@
+﻿using act_Application.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace act_Application.Controllers.General
+{
+    public class TablaAportantesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var arobj = new AportacionRepository().GetDataAportaciones();
+            return View(arobj);
+        }
+    }
+}
---
+++ act_Application/Controllers/General/TablaMultadosController.cs
@@ -0,0 +1,16 @@
+﻿using act_Application.Data.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace act_Application.Controllers.General
+{
+    public class TablaMultadosController : Controller
+    {
+        [Authorize(Policy = "AdminSocioOnly")]
+        public IActionResult Index()
+        {
+            var mrobj = new MultaRepository().GetDataMultas();
+            return View(mrobj);
+        }
+    }
+}
