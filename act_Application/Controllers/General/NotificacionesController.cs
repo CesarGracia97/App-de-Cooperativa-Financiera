8c2dbcdc1070dca22aeddb206b8d1aa01dacc6f8
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-11-29 17:08:49 -0500

    Objetivo 4 - Vistas. 1. Bases de ViewModel

--- act_Application/Controllers/General/NotificacionesController.cs
+++ act_Application/Controllers/General/NotificacionesController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace act_Application.Controllers.General
 {
     public class NotificacionesController : Controller
     {
+        [Authorize(Policy = "AllOnly")]
         public IActionResult Index()
         {
             return View();
---
+++ act_Application/Models/Sistema/ViewModel/Notificaciones_VM.cs
@@ -0,0 +1,22 @@
+﻿using act_Application.Models.BD;
+
+namespace act_Application.Models.Sistema.ViewModel
+{
+    public class Notificaciones_VM
+    {
+        public Notificaciones_VM()
+        {
+            Aportaciones = new ActAportacione();
+            Prestamos = new ActPrestamo();
+            Cuotas = new ActCuota();
+            Multas = new ActMulta();
+            Notificaciones = new ActNotificacione();
+        }
+        public ActNotificacione Notificaciones { get; set; }
+        public ActPrestamo Prestamos { get; set; }
+        public ActCuota Cuotas { get; set; }
+        public ActAportacione Aportaciones { get; set; }
+        public ActEvento Eventos { get; set; }
+        public ActMulta Multas { get; set; }
+    }
+}
