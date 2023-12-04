801f7587e27df71812d404325aa7ca17b77cb401
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-12-04 08:40:35 -0500

    Objetivo 4 - Vistas. 1. Metodo Clase Complemento Logico "ObtenerTipoNotificacion".

---
+++ act_Application/Logic/ComplementosLogicos/ObtenerTipoNotificacion.cs
@@ -0,0 +1,51 @@
+﻿namespace act_Application.Logic.ComplementosLogicos
+{
+    public class ObtenerTipoNotificacion
+    {
+        public static string ClasificarInformacion(string cadena)
+        {
+            if (cadena.Length >= 4)
+            {
+                // Verificar si los primeros 4 caracteres son alfabéticos seguidos por un guion
+                if (char.IsLetter(cadena[0]) && char.IsLetter(cadena[1]) && char.IsLetter(cadena[2]) && char.IsLetter(cadena[3]) && cadena[4] == '-')
+                {
+                    // Información de Tipo Actividad
+                    return ObtenerTipoActividad(cadena);
+                }
+
+                // Verificar si los primeros 4 caracteres son numéricos y la longitud es 10
+                if (char.IsDigit(cadena[0]) && char.IsDigit(cadena[1]) && char.IsDigit(cadena[2]) && char.IsDigit(cadena[3]) && cadena.Length == 10)
+                {
+                    // Información de Tipo Registro de Cuenta
+                    return "NUSE";
+                }
+            }
+
+            // Si no cumple ninguna condición, devolver un mensaje indicando que no se pudo clasificar
+            return "No se pudo clasificar la información";
+        }
+
+        private static string ObtenerTipoActividad(string cadena)
+        {
+            // Obtener los primeros 4 caracteres después del guion
+            string tipo = cadena.Substring(5, 4);
+
+            // Mapear los tipos y devolver el resultado correspondiente
+            switch (tipo)
+            {
+                case "APOR":
+                    return "APOR";
+                case "MULT":
+                    return "MULT";
+                case "CUOT":
+                    return "CUOT";
+                case "EVEN":
+                    return "EVEN";
+                case "PRES":
+                    return "PRES";
+                default:
+                    return "Tipo de actividad no reconocido";
+            }
+        }
+    }
+}
