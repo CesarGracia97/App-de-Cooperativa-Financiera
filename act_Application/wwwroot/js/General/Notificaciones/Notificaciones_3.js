5a8c3c801b5718ca68ae2c73cb291c039c61bf11
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-12-18 09:19:13 -0500

    Objetivo 4 - Vistas. 1. JS de aparicion en Formulario

---
+++ act_Application/wwwroot/js/General/Notificaciones/Notificaciones_3.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    // Obtener referencias a los elementos
+    var selectElement = document.querySelector('.select-acep-dene');
+    var sContainer = document.querySelector('.S-container-modal-btn');
+    var nContainer = document.querySelector('.N-container-modal-btn');
+
+    // Ocultar los contenedores por defecto
+    sContainer.style.display = 'none';
+    nContainer.style.display = 'none';
+
+    // Manejar el evento de cambio en el select
+    selectElement.addEventListener('change', function () {
+        // Ocultar todos los contenedores
+        sContainer.style.display = 'none';
+        nContainer.style.display = 'none';
+
+        // Mostrar el contenedor correspondiente según la opción seleccionada
+        if (selectElement.value === 'SI') {
+            sContainer.style.display = 'block';
+        } else if (selectElement.value === 'NO') {
+            nContainer.style.display = 'block';
+        }
+    });
+});
