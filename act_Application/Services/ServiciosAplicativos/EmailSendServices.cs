9c7be458b929bf98355557feee762559d8c5e176
CesarGracia97 <cesar.gracia@pucese.edu.ec>
2023-12-18 09:43:05 -0500

    Objetivo 4 - Vistas. 1. Correcion de Ruta JSON

--- act_Application/Services/ServiciosAplicativos/EmailSendServices.cs
+++ act_Application/Services/ServiciosAplicativos/EmailSendServices.cs
@@ -135,7 +135,7 @@ namespace act_Application.Services.ServiciosAplicativos
         /*Envia el Correo*/
         private async Task EnviarCorreo(string destinatario, string asunto, string mensaje)
         {
-            var smtpConfig = SmtpConfig.LoadConfig("Data/Config/smtp.json");
+            var smtpConfig = SmtpConfig.LoadConfig("Data/json/smtp.json");
 
             using (var smtpClient = new SmtpClient(smtpConfig.Server, smtpConfig.Port))
             {
